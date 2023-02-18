94cb2b2bd8c1c1109be5b02415f7fa18920e0627
hadeer ahmed <hader@192.168.155.166>
2023-02-18 02:59:53 +0200

    add cell folder and and all custom cell in splitView make all outlet of all piaroity in splitView that's make this view done without constrain

---
+++ Sports/Cells/teamsCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  teamsCollectionViewCell.swift
+//  Sports
+//
+//  Created by Hader on 17/2/23.
+//
+
+import UIKit
+
+class teamsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var teamNameLB: UILabel!
+    @IBOutlet weak var teamImg: UIImageView!
+}
